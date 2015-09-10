bdece836ba40223ef9f2b81b3c49875d68b11b76
Chien <chien@streamline.sg>
2015-09-10 12:01:00 +0700

    [FIX] requirement#15 dynamic calculate purchase qty and received qty

--- streamline_ame_modules/purchase/models/material.py
+++ streamline_ame_modules/purchase/models/material.py
@@ -21,9 +21,49 @@ class streamline_ame_material_take_off_line(models.Model):
     received_qty = fields.Float(compute='compute_received_qty', string="Total Received Qty")
 
     def compute_purchased_qty(self):
+        purchase_lines_list = {}
+        # get purchase line and calculate qty
+        po_model_list = self.env['purchase.order'].search([('project_no', '=', self[0].parent_id.project_no.id),'|',('state', '=', 'approved'), ('state', '=', 'done')])
+        for po in po_model_list:
+            for line in po.order_line:
+                if str(line.product_id.id) in purchase_lines_list:
+                    purchase_lines_list[str(line.product_id.id)] += line.product_qty
+                else:
+                    purchase_lines_list[str(line.product_id.id)] = line.product_qty
+        
         for record in self:
-            record.purchased_qty = 0.0
+            if str(record.product_id.id) in purchase_lines_list:
+                record.purchased_qty = purchase_lines_list[str(record.product_id.id)]
+            else:
+                record.purchased_qty = 0.0
         
     def compute_received_qty(self):
+        move_line_list = {}
+        # use query get stock_pciking
+        query = """
+        SELECT picking_id FROM stock_picking p, stock_move m, purchase_order_line pol, purchase_order po
+            WHERE po.id in %s and po.id = pol.order_id and pol.id = m.purchase_line_id and m.picking_id = p.id
+            GROUP BY picking_id, po.id
+        """
+        po_model_list = self.env['purchase.order'].search([('project_no', '=', self[0].parent_id.project_no.id),'|',('state', '=', 'approved'), ('state', '=', 'done')])
+        
+        self.env.cr.execute(query, (tuple([x.id for x in po_model_list]), ))
+        picks = self.env.cr.fetchall()
+        
+        a = ()
+        for pick in picks:
+            a += pick
+        
+        for pick_obj in self.env['stock.picking'].browse(a):
+            for move in pick_obj.move_lines:
+                if move.state == 'done':
+                    if str(move.product_id.id) in move_line_list:
+                        move_line_list[str(move.product_id.id)] += move.product_qty
+                    else:
+                        move_line_list[str(move.product_id.id)] = move.product_qty
+        
         for record in self:
-            record.received_qty = 0.0    
+            if str(record.product_id.id) in move_line_list:
+                record.received_qty = move_line_list[str(record.product_id.id)]
+            else:
+                record.received_qty = 0.0    
