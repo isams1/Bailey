b587185d53868c00b87222d865a878ba749f3131
Jack <lehoangan1988@gmail.com>
2016-02-17 16:22:15 +0700

    AME-Task 3 with dynamic footer

--- streamline_ame_modules/base/models/res_company.py
+++ streamline_ame_modules/base/models/res_company.py
@@ -4,3 +4,4 @@ class res_company(models.Model):
     _inherit = "res.company"
     
     letterhead = fields.Binary("LetterHead", help="This field holds the image used as Report Header")
+    letterfoot = fields.Binary("Letter Footer", help="This field holds the image used as Report Header")
